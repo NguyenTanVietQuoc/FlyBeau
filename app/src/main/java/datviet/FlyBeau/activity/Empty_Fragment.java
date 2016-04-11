e50445edd8286e95e78ef2851b8f505aadf07fef
nthanhnambmt <nthanhnambmt@gmail.com>
2016-04-11 09:17:24 +0700

    Commit 1

---
+++ app/src/main/java/datviet/FlyBeau/activity/Empty_Fragment.java
@@ -0,0 +1,24 @@
+package datviet.FlyBeau.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import datviet.FlyBeau.R;
+
+/**
+ * Created by NamNgo on 09/03/2016.
+ */
+public class Empty_Fragment extends Fragment {
+    public Empty_Fragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.layout_empty, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}//
---
+++ app/src/main/java/datviet/FlyBeau/activity/LoginFragment.java
@@ -0,0 +1,74 @@
+package datviet.FlyBeau.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import datviet.FlyBeau.R;
+
+/**
+ * Created by NamNgo on 10/04/2016.
+ */
+public class LoginFragment  extends Fragment{
+    EditText etUser, etPass;
+    Button btLogin, btRegister;
+//
+    public LoginFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        init(v);
+
+        return v;
+    }//END onCreateView
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }, 600);
+
+    }
+
+    public  void init(View v)
+    {
+        etUser = (EditText) v.findViewById(R.id.etUser);
+        etUser.requestFocus();
+        etPass = (EditText) v.findViewById(R.id.etPass);
+        btLogin = (Button) v.findViewById(R.id.btLogin);
+        btRegister = (Button) v.findViewById(R.id.btRegister);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Login", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((LoginActivity)getActivity()).loadFragment("register");
+            }
+        });
+    }
+
+}//END
